ba25a83c015ab3fb3c2591dd049722f8425275e9
karkem <karlkemper@web.de>
2024-02-06 23:30:31 +0100

    Bluetooth, stepper, nrf,
    Bluetooth, stepper, nrf working together with base station and vessel controll!
    ...GPS library needs to be changed!

--- BaseStation/BaseStation.ino
+++ BaseStation/BaseStation.ino
@@ -3,12 +3,71 @@
 //and forward the comunication via Bluetooth/Wifi/mqtt over Internet to a near by tablett 
 //on which an app will be used to controll the boat
 
+# include <BluetoothSerial.h>
+# include <SPI.h>
+# include <nRF24L01.h>
+# include <RF24.h>
+
+//Variables for nRF24
+RF24 radio(14, 27); // CE, CSN
+const byte address[6] = "00001";
+
+// Variables for Bluetooth
+BluetoothSerial SerialBT; //Create Bluetooth object
+
+int variableA = 0; // Initialize variables for the bluetooth input
+int variableB = 0;
+int variableC = 0;
+
+
 void setup() {
-  // put your setup code here, to run once:
+  //Set up Bluetooth
+  Serial.begin(9600); //Start serial monitor
+  SerialBT.begin("Arduino_bluetooth"); //Start bluetooth client
+
+  //Set up nrf24 Radio
+  radio.begin();
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.stopListening();
 
 }
 
+int number = 0; //only for testuing the nrf24!
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  //Read Bluetooth data 
+  if (SerialBT.available()) {
+    String receivedData = SerialBT.readStringUntil('br');
+    if (receivedData.charAt(0) == 'a') {
+      // If the string starts with 'a', store it in variableA
+      variableA = receivedData.substring(1).toInt();
+      Serial.print("Received 'a': ");
+      Serial.println(variableA);
+    } else if (receivedData.charAt(0) == 'b') {
+      // If the string starts with 'b', store it in variableB
+      variableB = receivedData.substring(1).toInt();
+      Serial.print("Received 'b': ");
+      Serial.println(variableB);
+    } else if (receivedData.charAt(0) == 'c') {
+      // If the string starts with 'c', store it in variableB
+      variableC = receivedData.substring(1).toInt();
+      Serial.print("Received 'c': ");
+      Serial.println(variableC);
+    }
+      char text[9];
+  sprintf(text, "%d", variableA);
+  Serial.println(text);
+  radio.write(&text, sizeof(text));
+  //Serial.println(variableA);
+  //radio.write(&variableA, sizeof(variableA));
+  delay(1000);
+  }
+
+  //Write bluetooth data over radio to the MRV
+
+
+  //number += 1;
+
 
 }
--- VesselControl/VesselControl/VesselControl.ino
+++ VesselControl/VesselControl/VesselControl.ino
@@ -1,12 +1,12 @@
-//This is the sketsch, that is running on the esp32 on the Boat.
+//This is the sketsch, that is running on the esp32 on the Boat. (WROOM 32)
 //the components are:
 //The ESP32
-//GPS module
+//GPS module: 1,3 (rx,tx)
 //Sonar Pin: 16,17(rx,tx) + 5V external power
 //2x ESC with DC Motor - Pin:25,26,gnd + 4V external power
-//Stepper
+//Stepper 12, 14, 27, 26
 //Contact to calibrate stepper
-//nrf24 Pin: 14,27 + 3V external power
+//nrf24 Pin: 25,33, + 3V external power + 23,19,18
 //Thermometer
 //SD card reader 
 
@@ -16,6 +16,9 @@
 # include <nRF24L01.h> //For nrf24
 # include <RF24.h> //For nrf24
 # include <TinyStepper_28BYJ_48.h> //for stepper
+# include <TinyStepper.h> //alternative to above!
+# include "neo6mGPS.h" //For the GPS Module
+
 
 
 //variables for sonar
@@ -29,7 +32,8 @@ Servo escL; //Left motor
 Servo escR; //Right Motor
 
 //variables for nrf24
-RF24 radio(14, 27); // CE, CSN
+char text[32] = "";
+RF24 radio(25, 33); // CE, CSN
 const byte address[6] = "00001";
 
 //variables for stepper
@@ -39,7 +43,26 @@ const int MOTOR_IN3_PIN = 6;
 const int MOTOR_IN4_PIN = 5;
 
 const int STEPS_PER_REVOLUTION = 2048;
-TinyStepper_28BYJ_48 stepper; // create the stepper motor object
+//TinyStepper_28BYJ_48 stepper; // create the stepper motor object
+
+//OR
+int location = 0;
+int go = 0;
+int destination = 0;
+
+// Define Arduino Pin Outputs
+#define IN1 12
+#define IN2 14
+#define IN3 27
+#define IN4 26
+#define HALFSTEPS 4096  // Number of half-steps for a full rotation
+
+// Init the TinyStepper Class
+TinyStepper stepper(HALFSTEPS, IN1, IN2, IN3, IN4);
+
+//variables for GPS
+neo6mGPS GPS;
+
 
 
 void setup() {
@@ -55,18 +78,37 @@ void setup() {
   SonarSerial.begin(115200);
 
   //Set up for nrf24
+  Serial.begin(9600);
   radio.begin();
   radio.openReadingPipe(0, address);
   radio.setPALevel(RF24_PA_MIN);
   radio.startListening();
+
+  //Setup for stepper
+  stepper.Enable();
+
+  //Setup GPS
+  GPS.begin(Serial1);
 
 
   delay(2000);
 
 }
 
+
+
+
 void loop() {
   //read radio command
+  read_radio();
+
+  controle_stepper();
+
+  read_sonar();
+
+  read_GPS();
+
+  delay(2000);
   //if there is a manual mode flag:
     //switch to manual mode:
     //direct controll off the two engins and the winch
@@ -82,7 +124,64 @@ void loop() {
         //when winch is back up: go to next waypoint 
       
 
+}
 
-  
+void read_sonar(){
+  SonarSerial.write(COM);
+  delay(100);
+  if(SonarSerial.available() > 0){
+    delay(4);
+    if(SonarSerial.read() == 0xff){    
+      buffer_RTT[0] = 0xff;
+      for (int i=1; i<4; i++){
+        buffer_RTT[i] = SonarSerial.read();   
+      }
+      CS = buffer_RTT[0] + buffer_RTT[1]+ buffer_RTT[2];  
+      if(buffer_RTT[3] == CS) {
+        int Distance = (buffer_RTT[1] << 8) + buffer_RTT[2];
+        Serial.print("Distance:");
+        Serial.print(Distance);
+        Serial.println("mm");
+      }
+    }
+  }
+}
 
+void read_GPS(){
+    if(GPS.available())
+  {
+    //Serial.print(GPS.utc_year);  Serial.print(" | ");
+    //erial.print(GPS.utc_month); Serial.print(" | ");
+    //Serial.print(GPS.utc_day);   Serial.print(" | ");
+    //Serial.print(GPS.utc_hour);  Serial.print(" | ");
+    //Serial.print(GPS.utc_min);   Serial.print(" | ");
+    //Serial.print(GPS.utc_sec);   Serial.print(" | ");
+    //Serial.print(GPS.lat_dd);    Serial.print(" | ");
+    //Serial.print(GPS.lon_dd);    Serial.print(" | ");
+    //Serial.print(GPS.sog_knots); Serial.print(" | ");
+    //Serial.print(GPS.cog_true);  Serial.print(" | ");
+    //Serial.print(GPS.navStatus);
+    //Serial.println();
+  }
+}
+
+void controle_stepper(){
+  go = destination - location;
+  Serial.print(" go: ");
+  Serial.print(go);
+  Serial.print(" destination: ");
+  Serial.print(destination);
+  Serial.print(" location: ");
+  Serial.println(location);
+  stepper.Move(go);
+  location = destination;
+}
+
+void read_radio(){
+    if (radio.available()) {
+    radio.read(&text, sizeof(text));
+    Serial.println("recieved:");
+    Serial.println(text);
+    destination = atoi(text);
+  }
 }
